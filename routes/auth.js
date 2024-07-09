77283cefe21323d4ba3bf83338d48faca5c05bd8
illusion8778 <mohammadikbal02@myhunter.cuny.edu>
2024-07-09 14:49:21 -0400

    Initial commit for backend code

---
+++ routes/auth.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+// Signup Route
+router.post('/signup', async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = new User({ email, password });
+    await user.save();
+    res.status(201).send('User registered');
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
+
+// Login Route
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).send('Invalid email or password');
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).send('Invalid email or password');
+
+    const token = jwt.sign({ userId: user._id }, 'YOUR_JWT_SECRET');
+    res.send({ token });
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
+
+module.exports = router;

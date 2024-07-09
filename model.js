bbaf30a3bc1efa8cc8a246d28b7d7b7507fb2be4
aleena.t <38635405+AleenaTim@users.noreply.github.com>
2024-07-08 23:52:39 -0400

    Updated model.js with correct properties corresponding to frontend design

--- model.js
+++ model.js
@@ -1,16 +1,44 @@
 const mongoose = require('mongoose');
+// User Schema
 
-var schema = new mongoose.Schema({
-    users : {
-        type : String,
-        required: true
-    },
-    recipes : {
-        type: String,
-        required: true,
-    }
-})
+const users = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  message: {
+    type: String,
+    required: false
+  }
+});
 
-const Userdb = mongoose.model('userdb', schema);
+// Recipe Schema
+const recipes = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  message: {
+    type: String,
+    required: false
+  }
+});
 
-module.exports = Userdb;
+// Models
+const User = mongoose.model('User', users);
+const Recipe = mongoose.model('Recipe', recipes);
+
+module.exports = {
+  User,
+  Recipe
+};
+

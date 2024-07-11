c12bb97361f1e2eeb5318f37c74ba17f750da83c
illusion8778 <mohammadikbal02@myhunter.cuny.edu>
2024-07-11 15:49:50 -0400

    Updated Environment Variable

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -13,7 +13,7 @@ function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://localhost:5000/api/login', { email, password });
+      const response = await axios.post('https://recipe-sharing-1.onrender.com/login', { email, password }); // http://localhost:5000/api/login
       login(response.data.token);
       navigate('/recipes');
     } catch (error) {

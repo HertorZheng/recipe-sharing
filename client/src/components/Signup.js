2a6cfb4bf04e17a11180abf96a4790906a1bd469
illusion8778 <mohammadikbal02@myhunter.cuny.edu>
2024-07-11 17:50:34 -0400

    Reverted the codes so the Website works

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -11,7 +11,7 @@ function Signup() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post('https://recipe-sharing-1.onrender.com/api/signup', { email, password }); //ttp://localhost:5000/api/signup
+      await axios.post('http://localhost:5000/api/signup', { email, password }); 
       navigate('/login');
     } catch (error) {
       setError('Error registering user');

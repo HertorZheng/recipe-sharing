51d5d1d7b2a0ca9ce071e20f46fc08e14962388c
illusion8778 <mohammadikbal02@myhunter.cuny.edu>
2024-07-14 00:12:14 -0400

    Testing if moving the client to the server and editing code works

--- server/index.js
+++ server/index.js
@@ -42,7 +42,4 @@ db.once('open', () => {
 });
 
 // Start server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.listen(5000, "localhost", () => console.log("Listening to port 5000"));

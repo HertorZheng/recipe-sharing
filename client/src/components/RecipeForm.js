07f7fb86dcb0bbe7311ff62ed3ce00bfd394fc18
illusion8778 <mohammadikbal02@myhunter.cuny.edu>
2024-07-14 13:36:44 -0400

    Fixed Signup Form border and most of the responsiveness seem to work and added form border to RecipeForm

--- client/src/components/RecipeForm.js
+++ client/src/components/RecipeForm.js
@@ -30,18 +30,23 @@ function RecipeForm() {
     }
   };
 
+
+
+
   return (
-    <form onSubmit={handleSubmit}>
-      <h2>Submit a Recipe</h2>
-      {message && <p>{message}</p>}
-      <label>Title:</label>
-      <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} required />
-      <label>Description:</label>
-      <textarea value={description} onChange={(e) => setDescription(e.target.value)} rows="10" cols="50" required />
-      <label>Image:</label>
-      <input type="file" onChange={(e) => setImage(e.target.files[0])} required />
-      <button type="submit">Submit</button>
-    </form>
+    <div className="form-border">
+      <form onSubmit={handleSubmit}>
+        <h2>Submit a Recipe</h2>
+        {message && <p>{message}</p>}
+        <label>Title:</label>
+        <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} required />
+        <label>Description:</label>
+        <textarea value={description} onChange={(e) => setDescription(e.target.value)} rows="10" cols="50" required />
+        <label>Image:</label>
+        <input type="file" onChange={(e) => setImage(e.target.files[0])} required />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   );
 }
 

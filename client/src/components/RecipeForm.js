0c2a5b06bea8a304cd6e01e740f9ca02af438ccc
illusion8778 <mohammadikbal02@myhunter.cuny.edu>
2024-07-14 14:22:35 -0400

    Made Changes to the RecipeForm Responsiveness

--- client/src/components/RecipeForm.js
+++ client/src/components/RecipeForm.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import "./RecipeForm.css";
 
 function RecipeForm() {
   const [title, setTitle] = useState('');
@@ -41,7 +42,7 @@ function RecipeForm() {
         <label>Title:</label>
         <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} required />
         <label>Description:</label>
-        <textarea value={description} onChange={(e) => setDescription(e.target.value)} rows="10" cols="50" required />
+        <textarea value={description} onChange={(e) => setDescription(e.target.value)} rows="10" cols="40" required />
         <label>Image:</label>
         <input type="file" onChange={(e) => setImage(e.target.files[0])} required />
         <button type="submit">Submit</button>

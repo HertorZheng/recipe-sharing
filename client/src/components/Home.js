e7cf2b71fbe4b60030347a9181444c6bc6495a4e
illusion8778 <mohammadikbal02@myhunter.cuny.edu>
2024-07-14 13:14:54 -0400

    Fixed Signup Form border and most of the responsiveness seem to work

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,7 +4,7 @@ function Home() {
   return (
     <div>
       <h2>Welcome to Recipe Sharing Website</h2>
-      <img src= 'https://m.media-amazon.com/images/I/71z11sktWBL._AC_UF1000,1000_QL80_.jpg' height="600pxw" alt="Home-Icon"></img>
+      <img src= 'https://m.media-amazon.com/images/I/71z11sktWBL._AC_UF1000,1000_QL80_.jpg' height="400pxw" alt="Home-Icon"></img>
     </div>
 
   );
